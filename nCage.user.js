9317d4aa6f858671bc6d49219aaeb5005a07371a
Thauã Corrêa Martins <thaua-martins@hotmail.com>
2018-07-11 13:33:38 -0300

    Create nCage.user.js

---
+++ nCage.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http*://*/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js/
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js/
+// @require      https://raw.githubusercontent.com/correamth/nCage/master/ncage.js
+// @grant        none
+// ==/UserScript==
